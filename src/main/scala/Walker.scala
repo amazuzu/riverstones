45d3b59516652bdefd3666247df72d5c39e2cfad
taras <amazuzu@gmail.com>
2016-09-11 01:38:59 +0300

    why not to make it even more simple

--- src/main/scala/Walker.scala
+++ src/main/scala/Walker.scala
@@ -7,21 +7,15 @@ object Walker extends App {
 
   def walkStart(v: Int, path: List[Char]) = walk(v, '#' :: path /* add stone since left bank is like s stone*/)
 
-  private def walk(v: Int, path: List[Char]): Iterator[List[Int]] = {
-    def fork(velocity: Int, fpath: List[Char]): Iterator[List[Int]] =
-      range(-1, 2).filter(_ + velocity > 0).flatMap { delta =>
-        (
-          if (velocity + delta >= fpath.length) single(Nil) //jump through river
-          else fpath.drop(velocity + delta) match {
-            case '#' :: Nil => single(List(0)) //last stone
-            case list@('#' :: _) => walk(velocity + delta, list) //we on stone
-            case _ => empty
-          }
-          ).map(delta :: _)
-      }
-
-    fork(v, path)
-  }
+  private def walk(v: Int, path: List[Char]): Iterator[List[_]] =
+    range(-1, 2) filter (_ + v > 0) flatMap { delta =>
+      (if (v + delta >= path.length) single(Nil) //jump through river
+      else path drop (v + delta) match {
+        case '#' :: Nil => single(List(0)) //last stone
+        case list@('#' :: _) => walk(v + delta, list) //we on stone
+        case _ => empty
+      }) map (delta :: _)
+    }
 
 
   println(walkStart(8, "#--#---#--#----#--#".toList).toList)
